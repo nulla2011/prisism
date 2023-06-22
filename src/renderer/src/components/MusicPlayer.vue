53672eb38f4a9f80609ec9c15d111f79418813e0
nulla2011 <nulla@outlook.com>
2023-06-22 23:37:16 +0800

    feat: music volume

--- src/renderer/src/components/MusicPlayer.vue
+++ src/renderer/src/components/MusicPlayer.vue
@@ -2,7 +2,7 @@
   <div
     class="absolute mx-auto left-0 right-0 bottom-12 w-[500px] h-20 rounded-lg bg-transparent border-solid border-2 border-[#c0c0c0c0] p-1.5">
     <audio ref="audioPlayer" :src="url" autoplay></audio>
-    <div class="flex items-center w-full h-full bg-[#4e4e4ea0] rounded-lg backdrop-blur-sm">
+    <div class="flex items-center w-full h-full bg-[#6b6b6b83] rounded-lg backdrop-blur-md">
       <button class="mx-2 cursor-pointer" @click="playOrPause">
         <pause-one v-if="isPlaying" theme="outline" size="45" fill="#eee"></pause-one>
         <play v-else theme="outline" size="45" fill="#eee"></play>
@@ -10,18 +10,29 @@
       <div class="flex-1 h-full">
         <div class="h-8 font-HumStd px-3 leading-8 text-[#f5f5f5]">{{ title }}</div>
         <div class="w-full relative top-[-0.4rem] flex">
-          <a-slider class="flex-1" :max="duration" v-model:value="currentTime" :tooltipOpen="false"
-            @change="positionChange"></a-slider>
-          <span class="mx-2 text-[#f5f5f5] leading-8"><span>{{ formatTime(currentTime) }}</span>/<span>{{
-            formatTime(duration)
-          }}</span></span>
+          <a-config-provider :theme="{
+            token: {
+              colorFillSecondary: '#f5f5f5a0',
+              colorFillTertiary: '#f5f5f5c0',
+            },
+          }">
+            <a-slider class="flex-1" :max="duration" v-model:value="currentTime" :tooltipOpen="false"
+              @change="positionChange"></a-slider>
+          </a-config-provider>
+          <span class="mx-2 text-[#f5f5f5] leading-8">
+            <span>{{ formatTime(currentTime) }}</span>/<span>{{ formatTime(duration) }}</span>
+          </span>
         </div>
       </div>
+      <button @click="mute" class="ml-1 mr-2">
+        <volume-mute v-if="isMute" theme="outline" size="22" fill="#eee"></volume-mute>
+        <volume-notice v-else theme="outline" size="22" fill="#eee"></volume-notice>
+      </button>
     </div>
   </div>
 </template>
 <script setup lang="ts">
-import { Play, PauseOne } from '@icon-park/vue-next';
+import { Play, PauseOne, VolumeNotice, VolumeMute } from '@icon-park/vue-next';
 import { onMounted, ref, watch } from 'vue';
 const props = defineProps<{ url: string, title: string }>();
 
@@ -29,6 +40,7 @@ let audioPlayer = ref<HTMLMediaElement | null>();
 let isPlaying = ref(false);
 let duration = ref(0);
 let currentTime = ref(0);
+let isMute = ref(false);
 watch(() => props.url, () => {
   isPlaying.value = true;
   duration.value = Math.floor(audioPlayer.value!.duration);
@@ -41,12 +53,25 @@ onMounted(() => {
 const playOrPause = () => {
   if (isPlaying.value) {
     audioPlayer.value?.pause();
+    isPlaying.value = false;
   } else {
     audioPlayer.value?.play();
+    isPlaying.value = true;
   }
-  isPlaying.value = !isPlaying.value;
 };
 const positionChange = () => audioPlayer.value!.currentTime = currentTime.value;
 const formatTime = (time: number) => `${Math.floor(time / 60)}:${(time % 60).toString().padStart(2, '0')}`;
+const mute = () => {
+  if (audioPlayer.value) {
+    if (isMute.value) {
+      audioPlayer.value.volume = 1;
+      isMute.value = false;
+    } else {
+      audioPlayer.value.volume = 0;
+      isMute.value = true;
+    }
+  }
+
+}
 </script>
 <style lang="scss" scoped></style>
--- src/renderer/src/views/Music/index.vue
+++ src/renderer/src/views/Music/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-radio-group v-model:value="tab" size="large" button-style="solid" class="absolute top-4 right-2 select-none">
+  <a-radio-group v-model:value="tab" size="large" button-style="solid" class="absolute top-4 right-2 select-none z-10">
     <a-radio-button value="song">歌曲</a-radio-button>
     <a-radio-button value="bgm">BGM</a-radio-button>
     <a-radio-button value="others">其他</a-radio-button>
